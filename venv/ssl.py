04a1fe9e7e388d268cd47cb27e05741b201e6944
nirkai <kaizler@g.jct.ac.il>
2018-06-12 06:46:21 -0400

    ssl

--- venv/ssl.py
+++ venv/ssl.py
@@ -2,12 +2,39 @@
 
 import SimpleHTTPServer
 import SocketServer
+from Tkinter import *
+from tkMessageBox import *
+from time import sleep
 #import logging
 import os
 import argparse
 import signal
 import sys
 import re
+
+
+flag = False
+
+def answer():
+    #global window
+    global flag
+    #s.send_header('Location', 'https://www.google.com')
+    print "self in answer is:"
+    #print s.path
+    flag = False
+    #window.quit()
+
+#window = Tk()
+
+
+
+def callback():
+    time_to_sleep = 1
+    flushIPTables()
+    sleep(time_to_sleep * 60)
+    defineIPTable()
+    print "we on the line again!\n"
+
 
 def signal_handler(signal, frame):
     flushIPTables()
@@ -18,14 +45,29 @@ def signal_handler(signal, frame):
 def redirect_factory():
     class RedirectServer(SimpleHTTPServer.SimpleHTTPRequestHandler):
         def do_GET(self):
+            #global window
+            global flag
+
             str1 = str(self.headers)[6:str(self.headers).find('\r\n')]
             print str1
             self.send_response(301)
 
-            if is_ssl_verify(str1):
-                self.send_header('Location', 'https://' + str1)
-            elif is_ssl_verify('www.' + str1):
+
+            if is_ssl_verify('www.' + str1):
+                print "first"
                 self.send_header('Location', 'https://www.' + str1)
+            elif is_ssl_verify(str1):
+                print "second"
+                self.send_header('Location', 'https://' + str1)
+            else:
+
+                #Button(text='PAUSE', command=quit).pack()
+                #Button(text='OK', command=answer).pack()
+                print "else"
+                print self.path
+                self.send_header('Location', 'https://www.google.com')
+                #mainloop()
+                print "end gui"
 
             self.end_headers()
     return RedirectServer
@@ -40,10 +82,17 @@ def flushIPTables():
     os.system('sudo echo 0 > /proc/sys/net/ipv4/ip_forward')
 
 def is_ssl_verify(url):
-    os.system('echo QUIT | openssl s_client -connect %s:443 -showcerts > output.txt 2>/dev/null & sleep 0.5' % url)
-    if os.stat("output.txt").st_size == 0 is True:
+    os.system('echo QUIT | openssl s_client -connect %s:443 -showcerts > output.txt 2>/dev/null & sleep 2' % url)
+    if os.stat("output.txt").st_size == 0:
         return False
     return True
+
+def isNumber(port):
+  try:
+    int(port)
+    return True
+  except:
+    return False
 
 def main():
 
@@ -55,11 +104,13 @@ def main():
     port = myargs.port
     host = myargs.ip
 
-    if isinstance(port, int):
+    if not isNumber(port):
+        print "invalid port"
         sys.exit(0)
 
     is_valid_ip = re.match("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", host)
-    if not is_valid_ip:
+    if not is_valid_ip and host != '':
+        print "invalid ip"
         sys.exit(0)
 
 
